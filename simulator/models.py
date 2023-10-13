855a87e209987721bcf8b08b07b4a6bfca979b8b
RamiroCovian <ramiro.covian@gmail.com>
2023-10-13 14:10:36 -0300

    feat: estructuro DBManager

---
+++ simulator/models.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+
+class DBManager:
+    def __init__(self):
+        pass
+
+    def connect(self):
+        pass
+
+    def disconnect(self):
+        pass
+
+    def consultSQL(self):
+        pass
